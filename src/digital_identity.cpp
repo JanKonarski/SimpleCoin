162d622e60e71b45befe9a78d1f3ba57eaf9e9b6
Maciejeg <maciek.majek2@gmail.com>
2023-10-24 23:00:42 +0200

    feat: ecdsa key pair generator

--- src/digital_identity.cpp
+++ src/digital_identity.cpp
@@ -55,6 +55,53 @@ bool DigitalIdentity::DeriveSeedFromWords() {
     return true;
 }
 
+bool DigitalIdentity::GenerateECDSAKeyPair() {
+    // Create an EC_KEY structure and set it to use the secp256k1 curve (Bitcoin curve)
+    ecKeyPair_ = EC_KEY_new_by_curve_name(NID_secp256k1);
+
+    // Allocate memory for a BIGNUM to store the private key
+    BIGNUM* privateKey = BN_new();
+
+    // Convert the seed data into a BIGNUM private key
+    BN_bin2bn(seed_.data(), static_cast<int>(seed_.size()), privateKey);
+
+    // Set the private key in the EC_KEY structure
+    EC_KEY_set_private_key(ecKeyPair_, privateKey);
+
+    // Create an EC_POINT structure to store the public key
+    EC_POINT* publicKeyPoint = EC_POINT_new(EC_KEY_get0_group(ecKeyPair_));
+
+    // Compute the public key point by multiplying the private key with the base point on the curve
+    EC_POINT_mul(EC_KEY_get0_group(ecKeyPair_), publicKeyPoint, privateKey, NULL, NULL, NULL);
+
+    // Set the public key in the EC_KEY structure
+    EC_KEY_set_public_key(ecKeyPair_, publicKeyPoint);
+
+    // Get a const pointer to the private key BIGNUM
+    const BIGNUM* privKeyBN = EC_KEY_get0_private_key(ecKeyPair_);
+
+    // Get a const pointer to the public key EC_POINT
+    const EC_POINT* pubKeyPoint = EC_KEY_get0_public_key(ecKeyPair_);
+
+    // Convert the private key BIGNUM to a hexadecimal string
+    char* privKeyHex = BN_bn2hex(privKeyBN);
+
+    // Convert the public key EC_POINT to a hexadecimal string (uncompressed format)
+    char* pubKeyHex = EC_POINT_point2hex(EC_KEY_get0_group(ecKeyPair_), pubKeyPoint, POINT_CONVERSION_UNCOMPRESSED, NULL);
+
+    // Store the private and public keys as strings in the class
+    privateKey_ = privKeyHex;
+    publicKey_ = pubKeyHex;
+
+    // Free the memory allocated for the temporary hexadecimal strings
+    OPENSSL_free(privKeyHex);
+    OPENSSL_free(pubKeyHex);
+
+    // Return true to indicate successful key pair generation
+    return true;
+}
+
+
 const std::string& DigitalIdentity::GetPrivateKey() const {
     return privateKey_;
 }
